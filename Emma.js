74d037ad3e8fe850b73a39f6ecd51eb882a157bf
Thobbie Egunjobi <egunjobithobbie4@gmail.com>
2024-11-19 16:15:12 +0100

    Updated

--- Emma.js
+++ Emma.js
@@ -3,5 +3,17 @@ let sum = 0;
 for (let i = 0; i <number.length; i++) {
     sum += number[i]; 
 }
+
+let max = 0
+for (let i = 0; i < number.length; i++) {
+    if (number[i] > max) {
+        console.log("Current number", number[i], "is greater than the Max", max)
+        max = number[i];
+        console.log("Max is now ", max)
+    }else{
+        console.log("Current number", number[i], "is NOT greater than the Max", max, "therefore, Max remains the same.")
+    }
+}
+console.log(max);
 console.log(sum)
 console.log(sum/number.length)
